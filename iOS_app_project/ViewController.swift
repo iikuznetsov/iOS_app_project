4a41a77f68cafca4cab5d92daf05aaef2bc80820
Igor <igor_neposedov@mail.ru>
2019-12-08 02:25:07 +0300

    Initial Commit

---
+++ iOS_app_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_app_project
+//
+//  Created by Игорь Кузнецов on 08/12/2019.
+//  Copyright © 2019 Игорь Кузнецов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
