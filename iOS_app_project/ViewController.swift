c14f2394e929e8b2c1b3f50c987a243339f20d6a
Maria Karavaeva <karavaeva-masha@mail.ru>
2019-12-12 10:29:15 +0300

    Мне кажется я чет поломала, но я не уверена, а еще добавила иконки

--- iOS_app_project/ViewController.swift
+++ iOS_app_project/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading th
     }
 
     @IBAction func enterButtonAction(_ sender: UIButton) {
