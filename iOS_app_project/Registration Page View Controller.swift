24fa64b082908eb7edbf4573c4a1a579c06877f8
Maria Karavaeva <karavaeva-masha@mail.ru>
2019-12-16 15:59:43 +0300

    занимаюсь страничкой регистрации

---
+++ iOS_app_project/Registration Page View Controller.swift
@@ -0,0 +1,23 @@
+//
+//  Registration Page View Controller.swift
+//  iOS_app_project
+//
+//  Created by Мария Караваева on 16.12.2019.
+//  Copyright © 2019 Игорь Кузнецов. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class RegistrationPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Yellow.jpg")!)
+        // Do any additional setup after loading th
+    }
+    
+    
+    
+}
