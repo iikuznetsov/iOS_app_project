8f7ab3b809b8842b01d89d929168932e4a992da8
Maria Karavaeva <karavaeva-masha@mail.ru>
2020-03-17 16:48:56 +0300

    страница внутренняя, профила и редакции профиля

--- iOS_app_project/Main Page/MainPageProvider.swift
+++ iOS_app_project/Main Page/MainPageProvider.swift
@@ -12,16 +12,16 @@ struct MainPageProvider {
     static func get() -> [MainPage] {
         return [
             MainPage(name: "Маникюр", icon: #imageLiteral(resourceName: "manicure")),
-            MainPage(name: "Окрашивание", icon: #imageLiteral(resourceName: "creative dying")),
+            MainPage(name: "Креативное окрашивание", icon: #imageLiteral(resourceName: "creative dying")),
             MainPage(name: "Спа-процедуры", icon: #imageLiteral(resourceName: "spa")),
             MainPage(name: "Педикюр", icon: #imageLiteral(resourceName: "pedicure")),
             MainPage(name: "Косметология", icon: #imageLiteral(resourceName: "cosmetology")),
             MainPage(name: "Татуировки", icon: #imageLiteral(resourceName: "tattoo")),
-            MainPage(name: "Окрашивание2", icon: #imageLiteral(resourceName: "classic dying")),
-            MainPage(name: "Татуаж бровей", icon: #imageLiteral(resourceName: "brows")),
+            MainPage(name: "Классическое окрашивание", icon: #imageLiteral(resourceName: "classic dying")),
+            MainPage(name: "Архитектура бровей", icon: #imageLiteral(resourceName: "brows")),
             MainPage(name: "Пирсинг", icon: #imageLiteral(resourceName: "piercing")),
-            MainPage(name: "Реснички", icon: #imageLiteral(resourceName: "lashes")),
-            MainPage(name: "Электроэпиляция", icon: #imageLiteral(resourceName: "epilation")),
+            MainPage(name: "Наращивание ресниц", icon: #imageLiteral(resourceName: "lashes")),
+            MainPage(name: "Эпиляция", icon: #imageLiteral(resourceName: "epilation")),
             MainPage(name: "Солярий", icon: #imageLiteral(resourceName: "sunroom"))
         ]
     }
